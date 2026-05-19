090722264a9e2c10485499c191cea3fccc75de2c
Samaksh Arora <95543309+samaksh-arora@users.noreply.github.com>
2026-05-19 14:39:51 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        string1 = s
+        string2 = t
+
+        string1FrequencyMap = {}
+        string2FrequencyMap = {}
+
+        for letter in string1:
+            string1FrequencyMap[letter] = string1FrequencyMap.get(letter, 0) + 1
+        
+        for letter in string2:
+            string2FrequencyMap[letter] = string2FrequencyMap.get(letter, 0) + 1
+        
+        return string1FrequencyMap == string2FrequencyMap
